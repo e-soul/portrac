5cfc23324136c3bd1f6fd9496a718055ddb7c339
e-soul contrib1 <esoulorgcontrib1@gmail.com>
2025-01-26 11:28:20 +0100

    Update copyright statement.

--- Trac.cc
+++ Trac.cc
@@ -1,4 +1,4 @@
-/* Copyright 2009-2014 e-soul.org. All rights reserved.
+/* Copyright 2009-2025 e-soul.org. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided
  * that the following conditions are met:
